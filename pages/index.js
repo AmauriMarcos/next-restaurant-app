0502b56109f4684040924a61cc78ffda92152ad0
AmauriMarcos <amaurisantos.m@gmail.com>
2022-01-12 12:58:54 +0100

    add Layout component

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,18 @@
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.css';
+import Head from 'next/head';
+import Image from 'next/image'
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Yummy Pizza</title>
+        <meta name='description' content='Delivery pizza app'/>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+ 
       <h1>TEST</h1>
+     
     </div>
   )
 }
