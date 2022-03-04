54d19b5876187ba97a9da40f7c35a207fa7db9b7
AmauriMarcos <amaurisantos.m@gmail.com>
2022-03-04 12:52:21 +0100

    create admin page

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,21 @@
+import React, {useEffect} from 'react';
 import "../styles/globals.css";
 import Layout from "../components/Layout";
 import { Provider } from "react-redux";
 import store from "../redux/store";
 
 
+
 function MyApp({ Component, pageProps }) {
- 
+
   return (
+ 
     <Provider store={store} >
       <Layout>
         <Component {...pageProps} />
       </Layout>
     </Provider>
+
   );
 }
 
---
+++ pages/admin/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from '../../styles/Admin.module.css';
+
+const index = () => {
+  return (
+    <div className={styles.container}>
+       <div>
+          ADMIN PAGE
+       </div>
+    </div>
+  )
+}
+
+export default index
